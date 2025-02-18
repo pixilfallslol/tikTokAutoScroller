5264c5b26c3f197f8dc5e0f572a99123f906d4e7
pixilfallslol <yayitstoday22@gmail.com>
2025-02-17 21:42:51 -0800

    Add files via upload

--- autoScroll.py
+++ autoScroll.py
@@ -16,10 +16,11 @@ BUTTON_SHADE_DARK = (100,100,100)
 
 surface = pygame.display.set_mode((W_W, W_H))
 smallfont = pygame.font.SysFont('Comic Sans MS',35) 
-text = smallfont.render('start' , True , WHITE) 
+text = smallfont.render('start' , True , WHITE)
+text2 = smallfont.render('stop' , True , WHITE)  
 times = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-x = 2519
-y = 780
+x = 1859
+y = 610
 startPressed = False
 stopPressed = False # TODO: Add stop logic and fix issue with gui failing.
 
@@ -30,9 +31,11 @@ def clickAfterTime():
             print(timeSleep)
             time.sleep(timeSleep)
             pyautogui.click(x, y)
-            print('clicked')   
-
+            print('clicked')  
+            
+            
 while True: 
+    
     for ev in pygame.event.get():
           
         if ev.type == pygame.QUIT: 
@@ -40,22 +43,37 @@ while True:
               
         if ev.type == pygame.MOUSEBUTTONDOWN: 
               
-            if W_W/2 <= mouse[0] <= W_W/2+140 and W_H/2 <= mouse[1] <= W_H/2+40:              
-                startPressed = True 
+            if W_W/2 <= mouse[0] <= W_W/2+140 and W_H/2 <= mouse[1] <= W_H/2+40:
+                clickAfterTime()
+                startPressed = True
+                print("Started")
                   
     surface.fill((WHITE)) 
       
     mouse = pygame.mouse.get_pos() 
- 
+    
+    # Start button.
     if W_W/2 <= mouse[0] <= W_W/2+140 and W_H/2 <= mouse[1] <= W_H/2+40: 
         pygame.draw.rect(surface,BUTTON_SHADE_LIGHT,[W_W/2,W_H/2,140,40]) 
           
     else: 
-        pygame.draw.rect(surface,BUTTON_SHADE_DARK,[W_W/2,W_H/2,140,40]) 
+        pygame.draw.rect(surface,BUTTON_SHADE_DARK,[W_W/2,W_H/2,140,40])
+        
+    
+    if W_W/5 <= mouse[0] <= W_W/5+140 and W_H/5 <= mouse[1] <= W_H/5+40:
+        startPressed = False
+        print("Stopped")
+
+    # Stop button.
+    if W_W/5 <= mouse[0] <= W_W/5+140 and W_H/5 <= mouse[1] <= W_H/5+40: 
+        pygame.draw.rect(surface,BUTTON_SHADE_LIGHT,[W_W/5,W_H/5,140,40]) 
+          
+    else: 
+        pygame.draw.rect(surface,BUTTON_SHADE_DARK,[W_W/5,W_H/5,140,40]) 
       
-    surface.blit(text , (W_W/2+50,W_H/2)) 
-            
-    clickAfterTime()
+    surface.blit(text, (W_W/2+50,W_H/2)) 
+    surface.blit(text2, (W_W/5+50,W_H/5))
+      
       
     pygame.display.update()
-           
+     
